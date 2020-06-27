30124366638a76b28bf942100b710b96160bccaa
Omar Hafez <hafezoa@gmail.com>
2020-06-26 22:03:18 -0500

    fetch dog breed data with axios and useEffect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 import './App.css';
 
 function App() {
+  const [data, setData] = useState({ breeds: [] });
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await axios(
+        'https://dog.ceo/api/breeds/list/all',
+      );
+      setData(response.data.message);
+      // console.log(response.data.message);
+    }
+    fetchData();
+  }, []);
 
   return (
     <div className="App">
-      <header className="App-header">
-        React Dog Breeds
-      </header>
+      <header className="App-header">React Dog Breeds</header>
+      <ul>{console.log(data)}</ul>
       <p>by Omar Hafez</p>
     </div>
   );
